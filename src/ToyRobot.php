39c07d76165f45c67a36865e1b3f69eff76fcd6c
Derek He <derek@xtom.com>
2020-07-10 16:42:26 +1000

    add readyToRun method and its exceptions

--- src/ToyRobot.php
+++ src/ToyRobot.php
@@ -2,18 +2,61 @@
 
 namespace App;
 
+use App\Exceptions\EmptyCommandsException;
+use App\Exceptions\NoPlaceCommandException;
+use App\Exceptions\InvalidTableSizeException;
+
 class ToyRobot
 {
     private int $tableSize;
     private array $commands;
+    private array $currentPosition;
 
     public function __construct(array $commands, $tableSize=5) {
         $this->commands = $commands;
         $this->tableSize = $tableSize;
+
+        $readyToRun = $this->readyToRun();
     }
 
     public function getCommands():array
     {
        return $this->commands;
+    }
+
+    public function run()
+    {
+       // initialise current position with first PLACE command
+       
+        
+    }
+
+    
+    public function readyToRun():bool
+    {
+        // 1. check if table size is greater than 0
+        $tableSize = $this->tableSize;
+        $validTableSize =(is_int($tableSize) && $tableSize >0) ? true : false;
+
+        if (!$validTableSize) {
+           throw new InvalidTableSizeException;
+        }
+        
+        // 2. check if commands array is empty
+        $notEmptyCommands = count($this->commands);
+        if (!$notEmptyCommands) {
+            throw new EmptyCommandsException;
+            
+        }
+
+        // 3. check if first command is PLACE command
+        $placePos = strpos(strtolower($this->commands[0]), 'place');
+        if ($placePos===false){
+             throw new NoPlaceCommandException;
+        }
+
+        return $validTableSize && $notEmptyCommands && ($placePos!==false) ? true : false;
+
+
     }
 }
--- tests/unit/ToyRobotTest.php
+++ tests/unit/ToyRobotTest.php
@@ -2,26 +2,52 @@
 
 use PHPUnit\Framework\TestCase;
 use App\Console;
+use App\Exceptions\EmptyCommandsException;
+use App\Exceptions\InvalidTableSizeException;
+use App\Exceptions\NoPlaceCommandException;
 use App\ToyRobot;
 
 class ToyRobotTest extends TestCase
 {
    protected $robot;
-   
+   protected $console;
+
    protected function setUp():void
    {
-    $console = new Console;
-    $console->readFile();
-    $commands = $console->getSlicedCommands();
+    $this->console = new Console;
+    $this->console->readFile();
+    $commands = $this->console->getSlicedCommands();
     $this->robot = new ToyRobot($commands);
    }
 
    /** @test */
-   public function has_commands()
+   public function ready_run()
    {
        $commands = $this->robot->getCommands();
-       var_dump($commands);
        $this->assertIsArray($commands);
+       $readyToRuN = $this->robot->readyToRun();
+       $this->assertEquals(true, $readyToRuN);
+   }
+
+   /** @test */
+   public function throw_exception_when_table_size_is_invalid()
+   {
+       $this->expectException(InvalidTableSizeException::class);
+       $this->robot = new ToyRobot($this->console->getSlicedCommands(), 0);
+   }
+
+    /** @test */
+   public function throw_exception_when_commands_is_empty()
+   {
+       $this->expectException(EmptyCommandsException::class);
+       $this->robot = new ToyRobot([]);
+   }
+
+   /** @test */
+   public function throw_exception_when_no_place_command()
+   {
+       $this->expectException(NoPlaceCommandException::class);
+       $this->robot = new ToyRobot(['MOVE', 'LEFT', 'MOVE']);
    }
 
 
